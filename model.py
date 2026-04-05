167c62b29b99e76d6c729900c263aa11c9bcae50
mwam0008 <mwambofritz1@gmail.com>
2026-04-04 22:38:40 -0400

    Update model.py

--- model.py
+++ model.py
@@ -6,7 +6,6 @@ Uses TensorFlow / Keras neural network for binary classification
 import logging
 import numpy as np
 import pandas as pd
-os.environ["TF_USE_LEGACY_KERAS"] = "1"
 import keras
 import tensorflow as tf
 from sklearn.preprocessing import StandardScaler
